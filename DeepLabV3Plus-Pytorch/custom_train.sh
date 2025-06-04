17aaf01bf33ce02027f9477090848575d6241bb6
fabian serkeyn <fserkeyn@tv115aiworkstation.clo.be>
2025-06-04 16:06:04 +0200

    last push from workstation after training

--- DeepLabV3Plus-Pytorch/custom_train.sh
+++ DeepLabV3Plus-Pytorch/custom_train.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # Train a model on your custom dataset
-python main.py \
+python ./DeepLabV3Plus-Pytorch/main.py \
   --dataset mydataset \
-  --data_root ./datasets/data/mydataset \
+  --data_root /mnt/Data1/fserkeyn/fserkeyn_deeplab_fine_train/fine-deeplab/_data/mydataset \
   --num_classes 6 \
   --model deeplabv3plus_mobilenet \
   --output_stride 16 \
-  --batch_size 8 \
-  --val_batch_size 4 \
-  --crop_size 512 \
+  --batch_size 16 \
+  --val_batch_size 8 \
+  --crop_size 640 \
   --crop_val \
   --lr 0.01 \
-  --total_itrs 20000 \
-  --val_interval 200 \
+  --total_itrs 1000 \
+  --val_interval 20 \
+  --print_interval 5 \
   --gpu_id 0 \
   --loss_type cross_entropy \
-  --save_val_results \
-  --ckpt checkpoints/your_checkpoint.pth
-
+  --ckpt checkpoints/your_checkpoint.pth \
+  --enable_vis \
+  --vis_env 'run2' \
+  --vis_port 13570 \
+  --save_val_results
--- DeepLabV3Plus-Pytorch/datasets/my_FINE_dataset.py
+++ DeepLabV3Plus-Pytorch/datasets/my_FINE_dataset.py
@@ -1,30 +1,66 @@
 import os
 from PIL import Image
 import numpy as np
+import torch
 import torch.utils.data as data
+import torchvision.transforms
 
 class MyDataset(data.Dataset):
     def __init__(self, root, split='train', transform=None):
         self.root = root
         self.split = split
         self.transform = transform
-        self.images = sorted(os.listdir(os.path.join(root, 'images')))
-        self.masks = sorted(os.listdir(os.path.join(root, 'masks')))
+
+        self.image_dir = os.path.join(root, 'images', split)
+        self.mask_dir = os.path.join(root, 'masks', split)
+
+        self.images = sorted(os.listdir(self.image_dir))
+        self.masks = sorted(os.listdir(self.mask_dir))
+
+        assert len(self.images) == len(self.masks), "Number of images and masks must match"
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.root, 'images', self.images[index])
-        mask_path = os.path.join(self.root, 'masks', self.masks[index])
+        img_path = os.path.join(self.root, 'images', self.split, self.images[index])
+        mask_path = os.path.join(self.root, 'masks', self.split, self.masks[index])
 
         image = Image.open(img_path).convert('RGB')
-        mask = Image.open(mask_path)
+        mask = Image.open(mask_path).convert('RGB')
 
         if self.transform:
             image, mask = self.transform(image, mask)
+        else:
+            image = torchvision.transforms.ToTensor()(image)
+
+        mask = self.rgb_to_class(mask)
+        mask = torch.from_numpy(mask).long()
 
         return image, mask
+
+    def rgb_to_class(self, mask):
+        """
+        Converts an RGB mask (H x W x 3) to class indices (H x W).
+        """
+        cmap = np.array([
+            [0, 0, 0],         # background
+            [250, 50, 83],     # Groenten
+            [52, 209, 183],    # Samengesteld
+            [50, 183, 250],    # Saus
+            [250, 250, 55],    # Vlees/Vis
+            [102, 255, 102],   # Zetmeel
+        ], dtype=np.uint8)
+
+        mask_np = np.array(mask)
+        h, w, _ = mask_np.shape
+        class_mask = np.zeros((h, w), dtype=np.int64)  # to store class indices
+
+        for i, color in enumerate(cmap):
+            matches = np.all(mask_np == color, axis=-1)
+            class_mask[matches] = i
+
+        return class_mask
 
     @classmethod
     def decode_target(cls, mask):
